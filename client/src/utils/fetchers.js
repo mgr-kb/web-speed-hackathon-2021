0c7f513fd160cd8c69bb8be0f39f53976ccf5cb8
mgr <koumgrars13@gmail.com>
2021-12-18 11:42:16 +0900

    remove jquery

--- client/src/utils/fetchers.js
+++ client/src/utils/fetchers.js
@@ -5,14 +5,8 @@ import { gzip } from 'pako';
  * @returns {Promise<ArrayBuffer>}
  */
 async function fetchBinary(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'binary',
-    method: 'GET',
-    responseType: 'arraybuffer',
-    url,
-  });
-  return result;
+  const result = await fetch(url);
+  return result.arrayBuffer();
 }
 
 /**
@@ -21,13 +15,8 @@ async function fetchBinary(url) {
  * @returns {Promise<T>}
  */
 async function fetchJSON(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'json',
-    method: 'GET',
-    url,
-  });
-  return result;
+  const result = await fetch(url);
+  return result.json();
 }
 
 /**
@@ -37,18 +26,14 @@ async function fetchJSON(url) {
  * @returns {Promise<T>}
  */
 async function sendFile(url, file) {
-  const result = await $.ajax({
-    async: false,
-    data: file,
-    dataType: 'json',
+  const result = await fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/octet-stream',
     },
-    method: 'POST',
-    processData: false,
-    url,
+    body: file,
   });
-  return result;
+  return result.json();
 }
 
 /**
@@ -62,19 +47,15 @@ async function sendJSON(url, data) {
   const uint8Array = new TextEncoder().encode(jsonString);
   const compressed = gzip(uint8Array);
 
-  const result = await $.ajax({
-    async: false,
-    data: compressed,
-    dataType: 'json',
+  const result = await fetch(url, {
+    method: 'POST',
     headers: {
       'Content-Encoding': 'gzip',
       'Content-Type': 'application/json',
     },
-    method: 'POST',
-    processData: false,
-    url,
+    body: compressed,
   });
-  return result;
+  return result.json();
 }
 
 export { fetchBinary, fetchJSON, sendFile, sendJSON };
