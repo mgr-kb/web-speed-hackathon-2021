4701f81d7844fd40306a2a456ba8e56af2fb9c62
mgr <koumgrars13@gmail.com>
2021-12-18 11:58:01 +0900

    fix webpack mode

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ const config = {
     },
     static: [PUBLIC_PATH, UPLOAD_PATH],
   },
-  devtool: 'inline-source-map',
+  devtool: process.env.NODE_ENV === 'production' ? false : 'inline-source-map',
   entry: {
     main: [
       'core-js',
@@ -61,7 +61,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
+      NODE_ENV: 'production',
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
